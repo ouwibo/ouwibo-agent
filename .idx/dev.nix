9c1dff7de5d02f9d8adf87c82556ac47af137273
ouwibo <contact@ouwibo.com>
2026-03-16 22:18:08 +0700

    chore: add idx configuration

---
+++ .idx/dev.nix
@@ -0,0 +1,41 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ritwickdey.LiveServer"
+      "esbenp.prettier-vscode"
+    ];
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npx" "http-server" "static" "-p" "$PORT"];
+          manager = "web";
+        };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a continuous build process
+        # build-watch = "npm run build:watch";
+      };
+    };
+  };
+}
