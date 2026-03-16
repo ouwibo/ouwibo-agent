0945a88979c5cfe6f21cdb2346f3aaeb1872603f
ouwibo <contact@ouwibo.com>
2026-03-16 22:32:41 +0700

    fix: correctly configure fastapi backend for idx

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,10 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.python311
+    pkgs.python311Packages.pip
     pkgs.nodejs_20
   ];
   # Sets environment variables in the workspace
@@ -14,13 +14,14 @@
     extensions = [
       "ritwickdey.LiveServer"
       "esbenp.prettier-vscode"
+      "ms-python.python"
     ];
     # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["npx" "http-server" "static" "-p" "$PORT"];
+          command = ["python" "-m" "uvicorn" "api:app" "--host" "0.0.0.0" "--port" "$PORT"];
           manager = "web";
         };
       };
@@ -29,7 +30,7 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        npm-install = "npm install";
+        python-install = "python -m venv venv && source venv/bin/activate && pip install -r requirements.txt";
       };
       # Runs when the workspace is (re)started
       onStart = {
